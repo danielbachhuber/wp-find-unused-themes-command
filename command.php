75391ac820f53f8202e0aa1156dd30f79c824edb
Daniel Bachhuber <daniel@handbuilt.co>
2016-03-03 14:19:14 -0800

    Initial commit

---
+++ command.php
@@ -0,0 +1,42 @@
+<?php
+
+if ( ! class_exists( 'WP_CLI' ) ) {
+	return;
+}
+
+/**
+ * Find unused themes on a multisite network.
+ *
+ * Iterates through all sites on a network to find themes which aren't enabled
+ * on any site.
+ */
+$find_unused_themes_command = function() {
+
+	$response = WP_CLI::launch_self( 'site list', array(), array( 'format' => 'json' ), false, true );
+	$sites = json_decode( $response->stdout );
+	$unused = array();
+	$used = array();
+	foreach( $sites as $site ) {
+		WP_CLI::log( "Checking {$site->url} for unused themes..." );
+		$response = WP_CLI::launch_self( 'theme list', array(), array( 'url' => $site->url, 'format' => 'json' ), false, true );
+		$themes = json_decode( $response->stdout );
+		foreach( $themes as $theme ) {
+			if ( 'no' == $theme->enabled && 'inactive' == $theme->status && ! in_array( $theme->name, $used ) ) {
+				$unused[ $theme->name ] = $theme;
+			} else {
+				if ( isset( $unused[ $theme->name ] ) ) {
+					unset( $unused[ $theme->name ] );
+				}
+				$used[] = $theme->name;
+			}
+		}
+	}
+	WP_CLI\Utils\format_items( 'table', $unused, array( 'name', 'version' ) );
+};
+WP_CLI::add_command( 'find-unused-themes', $find_unused_themes_command, array(
+	'before_invoke' => function(){
+		if ( ! is_multisite() ) {
+			WP_CLI::error( 'This is not a multisite install.' );
+		}
+	},
+) );
---
+++ features/find-unused-themes.feature
@@ -0,0 +1,35 @@
+Feature: Inspect WordPress multisite for unused themes
+
+  Background:
+    Given a WP multisite install
+    And I run `wp site create --slug=foo`
+    And I run `wp site create --slug=bar`
+
+  Scenario: Find unused themes on the network
+    When I run `wp theme install p2`
+    Then STDOUT should not be empty
+
+    When I run `wp find-unused-themes`
+    Then STDOUT should contain:
+      """
+      p2
+      """
+    And STDOUT should contain:
+      """
+      twentyfourteen
+      """
+
+    When I run `wp --url=example.com/foo theme enable p2`
+    Then STDOUT should not be empty
+
+    When I run `wp find-unused-themes`
+    Then STDOUT should not contain:
+      """
+      p2
+      """
+    And STDOUT should contain:
+      """
+      twentyfourteen
+      """
+
+
